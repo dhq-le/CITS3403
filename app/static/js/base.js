455f668be1e694dc187768ad4e8f80f60f662ea8
llullabyee <23101312@student.uwa.edu.au>
2025-04-25 22:04:41 +0800

    fix(sidebar): quality of life tweaks and fixed content loading

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,37 +1,44 @@
-function toggleSidebar() {
-  const sidebar = document.getElementById('sidebarMenu');
-  const menuButton = document.getElementById('menu-button');
-  const main = document.getElementById('main');
+document.addEventListener('DOMContentLoaded', () => {
+	const sidebar = document.getElementById('sidebarMenu');
+	const menuButton = document.getElementById('menu-button');
+	const menuIcon = document.getElementById('menu-icon');
+	const main = document.getElementById('main');
 
-  sidebar.classList.toggle('collapsed');
-  menuButton.classList.toggle('collapsed');
 
-  // Only adjust layout on desktop
-  if (window.innerWidth >= 768) {
-    main.classList.toggle('expanded');
-  }
-}
+	function toggleSidebar() {
+		sidebar.classList.toggle('collapsed');
+		menuButton.classList.toggle('collapsed');
+		menuIcon.classList.toggle('rotate');
 
-function setVhUnit() {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
+		// Only adjust layout on desktop
+		if (window.innerWidth >= 768) {
+			main.classList.toggle('expanded');
 
-window.addEventListener('resize', setVhUnit);
-window.addEventListener('load', setVhUnit);
+		}
+	}
 
-function updateIconName() {
-  const menuButton = document.getElementById("menu-button");
+	menuButton.addEventListener('click', toggleSidebar);
 
-  if (window.innerWidth < 768) {
-    menuButton.setAttribute('name', 'menu');  // Change to 'menu' if width is less than 768px
-  } else {
-    menuButton.setAttribute('name', 'chevrons-right');  // Revert to 'menu-alt-left' if width is greater than or equal to 768px
-  }
-}
 
-// Run the function when the page loads
-window.addEventListener('load', updateIconName);
+	function setVhUnit() {
+		let vh = window.innerHeight * 0.01;
+		document.documentElement.style.setProperty('--vh', `${vh}px`);
+	}
 
-// Run the function whenever the window is resized
-window.addEventListener('resize', updateIconName);
+	window.addEventListener('resize', setVhUnit);
+	window.addEventListener('load', setVhUnit);
+
+	function updateIconName() {
+		if (window.innerWidth < 768) { // This is for mobile vs desktop
+			menuIcon.setAttribute('name', 'menu');  // Change to 'menu' if width is less than 768px
+		} else {
+				menuIcon.setAttribute('name', 'chevrons-right');  // Revert to 'chevrons-right' if width is greater than or equal to 768px
+		}
+	}
+
+	// Run the function when the page loads
+	window.addEventListener('load', updateIconName);
+
+	// Run the function whenever the window is resized
+	window.addEventListener('resize', updateIconName);
+});
