31e338b2f061870211280162d77b6dab5f7c839c
llullabyee <23101312@student.uwa.edu.au>
2025-05-03 17:19:03 +0800

    added controllers.py

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 from app.blueprints import routes_blueprint
-from app.controllers import *
+from app.controllers import login, logout, index, profile, log_workout
 
 
 routes_blueprint.route('/login', methods=['GET', 'POST'])(login)
-routes_blueprint.route('/')(index)
 routes_blueprint.route('/logout')(logout)
+routes_blueprint.route('/')(index)
 routes_blueprint.route('/profile')(profile)
 routes_blueprint.route('/log', methods=['GET', 'POST'])(log_workout)
