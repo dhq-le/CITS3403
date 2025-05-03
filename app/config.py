a7d9a15465c1e7b1025f6fe4a66097ad441d6e02
mos <mozhassanein@gmail.com>
2025-05-03 23:53:32 +0800

    database stuff

--- app/config.py
+++ app/config.py
@@ -5,5 +5,4 @@ default_database_path = 'sqlite:///'+os.path.join(basedir, 'database.db')
 
 class Config:
 	SECRET_KEY = 'yoursecret-key'
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '../database.db') or default_database_path
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', default_database_path)
