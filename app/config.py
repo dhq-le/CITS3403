43d5d340cf4a8e8e2343f373c469a60ee287f280
RitchR <23940731@students.uwa.edu.au>
2025-05-13 22:33:15 +0800

    Selenium Testing Here attached below are the changes I made for the Selenium Testing, in which I created a test config for testing mode and then it will try to access 5 different tests: 1. Test the signup and creating account page 2. Test the dashboard access 3. Test ensuring that unauthenticated access to / triggers the login page 4. Test the profile page

--- app/config.py
+++ app/config.py
@@ -13,3 +13,11 @@ class Config:
     # SECRET_KEY = os.environ.get('SECRET_KEY', 'default-fallback-secret')
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', default_database_path)
+
+class TestingConfig(Config):
+    TESTING = True
+    SECRET_KEY = 'test-secret-key'
+    # Use an in-memory SQLite database for tests
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    # Disable CSRF for testing
+    WTF_CSRF_ENABLED = False
