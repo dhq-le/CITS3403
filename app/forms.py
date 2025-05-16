f873e67a6dd848e5c712bff4f6fdc4e8b3226124
llullabyee <23101312@student.uwa.edu.au>
2025-05-16 16:18:52 +0800

    fixed a broken validator.

--- app/forms.py
+++ app/forms.py
@@ -80,6 +80,7 @@ class EditProfileForm(FlaskForm):
                         r'^\w+$', message="Username must contain only letters, numbers, or underscores.")
         ])
         password = PasswordField('Password', validators=[
+                Optional(),
                 Length(min=8, message="Password must be at least 8 characters long.")
         ])
         height = IntegerField('Height (in cm)', validators=[
